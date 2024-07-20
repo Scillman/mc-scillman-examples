f05a4b0480fb655ec60591d10e31ac7d5761ebe2
Scillman <Scillman@users.noreply.github.com>
2024-07-20 20:22:12 +0200

    Development Environment

--- common/makelink.bat
+++ common/makelink.bat
@@ -2,8 +2,11 @@
 
 set COMMON_DIR=%~1
 
+mkdir wrapper
+
 mklink wrapper\gradle-wrapper.jar %COMMON_DIR%\wrapper\gradle-wrapper.jar
 mklink wrapper\gradle-wrapper.properties %COMMON_DIR%\wrapper\gradle-wrapper.properties
+
 mklink build.gradle %COMMON_DIR%\build.gradle
 mklink gradle.properties %COMMON_DIR%\gradle.properties
 mklink gradlew %COMMON_DIR%\gradlew
