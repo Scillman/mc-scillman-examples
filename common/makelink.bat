477bccc32c90f27ba66255090da05d0fb1b1805d
Scillman <Scillman@users.noreply.github.com>
2024-07-20 20:16:40 +0200

    Initial commit

---
+++ common/makelink.bat
@@ -0,0 +1,10 @@
+@echo off
+
+set COMMON_DIR=%~1
+
+mklink wrapper\gradle-wrapper.jar %COMMON_DIR%\wrapper\gradle-wrapper.jar
+mklink wrapper\gradle-wrapper.properties %COMMON_DIR%\wrapper\gradle-wrapper.properties
+mklink build.gradle %COMMON_DIR%\build.gradle
+mklink gradle.properties %COMMON_DIR%\gradle.properties
+mklink gradlew %COMMON_DIR%\gradlew
+mklink settings.gradle %COMMON_DIR%\settings.gradle
